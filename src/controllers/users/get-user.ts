4fced5cfb769aebe45d1804c164f987fca423495
Henrry Hernandez <jenrruhndz@gmail.com>
2022-02-26 14:13:59 -0700

    feat (users): add refreshed token when getting user info

--- src/controllers/users/get-user.ts
+++ src/controllers/users/get-user.ts
@@ -1,17 +1,23 @@
 import { Request, Response } from 'express';
 
+import { generateJWT } from '../../helpers/generate-jwt';
+
 import User from '../../models/user.model';
 
 export const getBasicUserInfoById = async (req: Request, res: Response) => {
   const { id } = req.params;
 
   try {
-    const user = await User.findOne({
+    const user: any = await User.findOne({
       attributes: ['id', 'name', 'lastname1', 'username', 'profilePicture'],
       where: { id },
     });
 
-    return res.status(200).json({ ok: true, error: null, msg: '', user });
+    const token = await generateJWT(user.id);
+
+    return res
+      .status(200)
+      .json({ ok: true, error: null, msg: '', user, token });
   } catch (error) {
     console.log(error);
 
