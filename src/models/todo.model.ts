423f334d44b8e56db5842b57f61fa8685a43388d
Henrry Hernandez <jenrruhndz@gmail.com>
2022-02-26 23:42:38 -0700

    refactor (todo model): allow title and description null

--- src/models/todo.model.ts
+++ src/models/todo.model.ts
@@ -8,8 +8,8 @@ const Todo = db.define(
   'Todo',
   {
     userId: { type: DataTypes.INTEGER, allowNull: false },
-    title: { type: DataTypes.STRING, allowNull: false },
-    description: { type: DataTypes.STRING, allowNull: false },
+    title: { type: DataTypes.STRING },
+    description: { type: DataTypes.STRING },
   },
   { tableName: 'Todos' }
 );
