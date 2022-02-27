faa5fa79b997397b53feb542a6ce1b60f6c6369e
Henrry Hernandez <jenrruhndz@gmail.com>
2022-02-27 01:17:42 -0700

    feat (todo routes): add validations for updating and deleting todos

--- src/middlewares/validate-user-allowed-to-access-info.ts
+++ src/middlewares/validate-user-allowed-to-access-info.ts
@@ -1,5 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 
+import Todo from '../models/todo.model';
+
 export const validateUserAuthorizedToAccessUserInfo = async (
   req: Request,
   res: Response,
@@ -8,6 +10,29 @@ export const validateUserAuthorizedToAccessUserInfo = async (
   const { userId, id } = req.params;
 
   if (+userId !== +id) {
+    return res.status(401).json({
+      ok: false,
+      error: null,
+      msg: 'User not authorized to access the information.',
+    });
+  }
+
+  next();
+};
+
+export const validateUserAuthorizedToAccessTodoInfo = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { todoId, id: userId } = req.params;
+
+  const todo: any = await Todo.findOne({
+    attributes: ['userId'],
+    where: { id: todoId },
+  });
+
+  if (!todo || +userId !== +todo.userId) {
     return res.status(401).json({
       ok: false,
       error: null,
--- src/routes/todos.route.ts
+++ src/routes/todos.route.ts
@@ -5,15 +5,25 @@ import { deleteTodo } from '../controllers/todos/delete-todo';
 import { getTodos } from '../controllers/todos/get-todos';
 import { updateTodo } from '../controllers/todos/update-todo';
 
+import { validateFields } from '../middlewares/validate-fields';
 import { validateJWT } from '../middlewares/validate-jwt';
+import { validateUserAuthorizedToAccessTodoInfo } from '../middlewares/validate-user-allowed-to-access-info';
 
 const router = Router();
 
 router.post('/create', [validateJWT], createNewTodo);
 
-router.put('/update/:todoId', [validateJWT], updateTodo);
+router.put(
+  '/update/:todoId',
+  [validateJWT, validateUserAuthorizedToAccessTodoInfo, validateFields],
+  updateTodo
+);
 
-router.delete('/delete/:todoId', [validateJWT], deleteTodo);
+router.delete(
+  '/delete/:todoId',
+  [validateJWT, validateUserAuthorizedToAccessTodoInfo, validateFields],
+  deleteTodo
+);
 
 router.get('/get', [validateJWT], getTodos);
 
