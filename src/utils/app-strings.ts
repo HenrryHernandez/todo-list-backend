c75869b4d9b766931bb0c6e3ef59813bad7b564f
Henrry Hernandez <jenrruhndz@gmail.com>
2022-02-28 00:08:23 -0700

    feat (images): add default image

--- src/utils/app-strings.ts
+++ src/utils/app-strings.ts
@@ -1,3 +1,7 @@
 import path from 'path';
 
 export const imagesPath = path.join(__dirname, '../../uploaded-images');
+export const defaultImagePath = path.join(
+  __dirname,
+  '../assets/default-image.jpeg'
+);
